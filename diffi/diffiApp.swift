44a986b6cdd0a7ff406dc6c532881db2e0feac78
Janyk Steenbeek <me@janyk.dev>
2026-03-25 21:52:49 +0100

    Initial Commit

---
+++ diffi/diffiApp.swift
@@ -0,0 +1,17 @@
+//
+//  diffiApp.swift
+//  diffi
+//
+//  Created by Janyk Steenbeek on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct diffiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
